34a7a572f96c34ca21a3cae3df4abd0fcfa4eee8
Variarmo <mogilnik.lera@gmail.com>
2021-02-04 22:53:29 +1000

    struct

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ItemList from './components/ItemList/ItemList';
+
+const App = () => (
+  <div>
+    <h1>Мои важные дела</h1>
+    <ItemList />
+  </div>);
+
+export default App;
+
