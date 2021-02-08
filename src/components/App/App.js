e452d20da9233227de06c8e8de7bffa994d87e22
Variarmo <mogilnik.lera@gmail.com>
2021-02-08 19:18:18 +1000

    prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ItemList from './components/ItemList/ItemList';
-import Item from './components/Item/Items';
-import Footer from './components/Footer/Footer';
+import ItemList from '../ItemList/ItemList';
+import Item from '../Item/Item';
+import Footer from '../Footer/Footer';
 
 const todoItem = 'Важное и срочное дело.'
 const App = () => (
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Item from './components/Item/Items';
+import Item from '../Item/Item';
 
 const ItemList = ({ todoItem }) => (
   <ul>
